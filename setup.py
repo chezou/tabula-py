c599d1ab23ba138f37423a5df2540c63453b98f6
Michiaki Ariga <chezou+github@gmail.com>
2016-09-10 19:48:44 +0900

    Add jar in setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
   version='0.1',
   description='Simple wrapper for tabula, read tables from PDF into DataFrame',
   author=__author__,
+  author_email='chezou@gmail.com',
   url='https://github.com/chezou/tabula-py',
   classifiers=[
     'Development Status :: 4 - Beta',
@@ -15,10 +16,12 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3 :: Only',
   ],
-  packages=find_packages(),
+  include_package_data=True,
+  packages=['tabula'],
+  package_data={'tabula': ['tabula-0.9.1-jar-with-dependencies.jar']},
   license='MIT License',
   keywords=['data frame', 'pdf', 'table'],
   install_requires=[
-    'pandas'
+    'pandas',
   ],
 )
