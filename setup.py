bf27662cacc4d37eba6bd4e7ad757ba7f3216072
Michiaki Ariga <chezou+github@gmail.com>
2016-12-03 12:45:50 +0900

    Add flake8 config

--- setup.py
+++ setup.py
@@ -3,24 +3,27 @@ from setuptools import setup, find_packages
 __author__ = 'Michiaki Ariga'
 
 setup(
-  name='tabula-py',
-  version='0.2.1',
-  description='Simple wrapper for tabula, read tables from PDF into DataFrame',
-  author=__author__,
-  author_email='chezou@gmail.com',
-  url='https://github.com/chezou/tabula-py',
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Topic :: Text Processing :: General',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 2.7',
-  ],
-  include_package_data=True,
-  packages=find_packages(),
-  license='MIT License',
-  keywords=['data frame', 'pdf', 'table'],
-  install_requires=[
-    'pandas',
-  ],
+    name='tabula-py',
+    version='0.2.1',
+    description='Simple wrapper for tabula, read tables from PDF into DataFrame',
+    author=__author__,
+    author_email='chezou@gmail.com',
+    url='https://github.com/chezou/tabula-py',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Text Processing :: General',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 2.7',
+    ],
+    include_package_data=True,
+    packages=find_packages(),
+    license='MIT License',
+    keywords=['data frame', 'pdf', 'table'],
+    setup_requires=[
+        'flake8',
+    ],
+    install_requires=[
+        'pandas',
+    ],
 )
