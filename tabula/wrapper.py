89791269adbb210974f5b73e92863ae332bb4c28
Michiaki Ariga <chezou+github@gmail.com>
2017-01-09 21:49:40 +0900

    Add comments of every methods

--- tabula/wrapper.py
+++ tabula/wrapper.py
@@ -26,29 +26,8 @@ def read_pdf_table(input_path, **kwargs):
     Args:
         input_path (str):
             File path of tareget PDF file.
-        options (str, optional):
-            Option string for tabula-java.
-        pages (str, int, :obj:`list` of :obj:`int`, optional):
-            An optional values specifying pages to extract from. It allows
-            `str`,`int`, :obj:`list` of :obj:`int`.
-            Example: '1-2,3', 'all' or [1,2]
-        guess (bool, optional):
-            Guess the portion of the page to analyze per page.
-        area (:obj:`list` of :obj:`float`, optional):
-            Portion of the page to analyze(top,left,bottom,right).
-            Example: [269.875,12.75,790.5,561]. Default is entire page
-        spreadsheet (bool, optional):
-            Force PDF to be extracted using spreadsheet-style extraction
-            (if there are ruling lines separating each cell, as in a PDF of an
-            Excel spreadsheet)
-        nospreadsheet (bool, optional):
-            Force PDF not to be extracted using spreadsheet-style extraction
-            (if there are ruling lines separating each cell, as in a PDF of an
-             Excel spreadsheet)
-        password (bool, optional):
-            Password to decrypt document. Default is empty
-        silent (bool, optional):
-            Suppress all stderr output.
+        kwargs (dict):
+            Dictionary of option for tabula-java. Details are shown in `build_options()`
 
     Returns:
         Extracted pandas DataFrame or list.
@@ -83,6 +62,20 @@ read_pdf = read_pdf_table
 
 
 def convert_from(input_path, output_path, **kwargs):
+    '''Convert tables from PDF into a file.
+
+    Args:
+        input_path (str):
+            File path of tareget PDF file.
+        output_path (str):
+            File path of output file.
+        kwargs (dict):
+            Dictionary of option for tabula-java. Details are shown in `build_options()`
+
+    Returns:
+        Extracted pandas DataFrame or list.
+    '''
+
     if output_path is None or len(output_path) is 0:
         raise AttributeError("'output_path' shoud not be None or empty")
 
@@ -108,6 +101,42 @@ def convert_from(input_path, output_path, **kwargs):
 
 
 def build_options(kwargs={}):
+    '''Build options for tabula-java
+
+    Args:
+        options (str, optional):
+            Raw option string for tabula-java.
+        pages (str, int, :obj:`list` of :obj:`int`, optional):
+            An optional values specifying pages to extract from. It allows
+            `str`,`int`, :obj:`list` of :obj:`int`.
+            Example: '1-2,3', 'all' or [1,2]
+        guess (bool, optional):
+            Guess the portion of the page to analyze per page.
+        area (:obj:`list` of :obj:`float`, optional):
+            Portion of the page to analyze(top,left,bottom,right).
+            Example: [269.875,12.75,790.5,561]. Default is entire page
+        spreadsheet (bool, optional):
+            Force PDF to be extracted using spreadsheet-style extraction
+            (if there are ruling lines separating each cell, as in a PDF of an
+            Excel spreadsheet)
+        nospreadsheet (bool, optional):
+            Force PDF not to be extracted using spreadsheet-style extraction
+            (if there are ruling lines separating each cell, as in a PDF of an
+             Excel spreadsheet)
+        password (bool, optional):
+            Password to decrypt document. Default is empty
+        silent (bool, optional):
+            Suppress all stderr output.
+        format (str, optional):
+            Format for output file or extracted object
+            CSV, TSV, JSON
+        output_path (str, optional):
+            Output file path. File format of it is depends on `format`.
+            Same as `--outfile` option of tabula-java.
+
+    Returns:
+        Built dictionary of options
+    '''
     __options = []
     options = kwargs.get('options', '')
     # handle options described in string for backward compatibility
