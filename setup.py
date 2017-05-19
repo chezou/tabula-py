1ead0319ed43360354fcbb1517cbc6df7f27b2dc
Michiaki Ariga <chezou+github@gmail.com>
2017-05-19 11:17:36 +0900

    Change test target from py35 to py36

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Topic :: Text Processing :: General',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 2.7',
     ],
