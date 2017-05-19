e8c22052b51421e9986b37f7264adbba4bfd89b5
Michiaki Ariga <chezou+github@gmail.com>
2017-05-19 12:53:18 +0900

    Bump up

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
+import os
+
+def read_file(filename):
+    filepath = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)), filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    else:
+        return ''
+
 
 __author__ = 'Michiaki Ariga'
 
 setup(
     name='tabula-py',
-    version='0.6.0',
+    version='0.6.1',
     description='Simple wrapper for tabula, read tables from PDF into DataFrame',
+    long_description=read_file('README.md'),
     author=__author__,
     author_email='chezou@gmail.com',
     url='https://github.com/chezou/tabula-py',
