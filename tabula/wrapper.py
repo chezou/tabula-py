7cacaffce06a8033f6b4f8c8d5cbbea8b421c0b4
Christian Beckmann <mail@christian-beckmann.net>
2018-03-02 16:00:10 +0100

    update to tabula 1.2.0

--- tabula/wrapper.py
+++ tabula/wrapper.py
@@ -18,7 +18,7 @@ import pandas as pd
 import numpy as np
 from .util import deprecated_option
 
-JAR_NAME = "tabula-1.0.1-jar-with-dependencies.jar"
+JAR_NAME = "tabula-1.2.0-jar.jar"
 JAR_DIR = os.path.abspath(os.path.dirname(__file__))
 JAR_PATH = os.path.join(JAR_DIR, JAR_NAME)
 
