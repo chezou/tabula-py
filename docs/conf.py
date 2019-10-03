7b2da9c03083f5256cec09380fef47e1e5b8af72
Aki Ariga <chezou+github@gmail.com>
2019-10-04 00:06:56 +0900

    doc: fix contents.rst not found error on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -112,3 +112,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Work around for contents.rst not found error
+# See also: https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = 'index'
