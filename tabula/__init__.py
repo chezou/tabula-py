e97e2e40cbdd5e1679cf869a017e000e11be18bd
Aki Ariga <chezou+github@gmail.com>
2023-02-20 11:57:30 -0800

    Fix lint

--- tabula/__init__.py
+++ tabula/__init__.py
@@ -1,4 +1,4 @@
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
 
 from .io import convert_into, convert_into_by_batch, read_pdf, read_pdf_with_template
 from .util import environment_info
